ea2359614fc87524f5179f1034cf68b60243b745
shinhosuck <shinhosuck1973@gmail.com>
2023-11-14 09:25:15 +0800

    update

--- about_me/urls.py
+++ about_me/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from about_me.views import index, my_projects, contact, about
+from about_me.views import (
+     landing_page_view, 
+     my_projects, 
+     contact, 
+     about
+)
 
 app_name = "about_me"
 
 
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", landing_page_view, name="landing-page"),
     path("my_projects/", my_projects, name="my_projects"),
     path("contact/", contact, name="contact"),
     path("about/", about, name="about"),
--- about_me/views.py
+++ about_me/views.py
@@ -5,16 +5,16 @@ from django.contrib import messages
 
 
 
-def index(request):
-    return render(request, "about_me/index.html", {})
+def landing_page_view(request):
+    return render(request, "about_me/landing_page.html")
 
 
 def my_projects(request):
-    return render(request, "about_me/my_projects.html", {})
+    return render(request, "about_me/my_projects.html")
 
 
 def about(request):
-    return render(request, "about_me/about.html", {})
+    return render(request, "about_me/about.html")
 
 
 def contact(request):
@@ -28,11 +28,3 @@ def contact(request):
             messages.warning(request, f"Message did not sent. Please try again!")
         return redirect("about_me:my_projects")
 
-
-# def view_img(request):
-#     if request.GET.get("string"):
-#         string = request.GET.get("string")
-#         img_dir = string
-#         print(img_dir)
-#         print(type(img_dir))
-#     return render(request, "about_me/view_img.html", {"img_dir": format_html(img_dir)})
